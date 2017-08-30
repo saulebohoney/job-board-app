31c9ac7cd998cbe3b04eda5457760e1aa2ef9d7b
Saule BN <saulebohoney@gmail.com>
2017-08-30 14:19:54 -0400

    removing the node modules from source

--- server/models.js
+++ server/models.js
@@ -1,19 +1,34 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-    googleId: {type: String, required: true},
-    name: {type: String, required: true},
-    accessToken: {type: String, required: true},
-});
+// const questionSchema = mongoose.Schema({
+//   question: {type: String, required: true},
+//   answer: {type: String, required: true}
+// });
 
-userSchema.methods.apiRepr = function() {
-    return {
-        name: this.name
-    };
-};
+// questionSchema.methods.apiRepr = function() {
+//   return {
+//     question: this.question,
+//     answer: this.answer
+//   };
+// };
 
-const User = mongoose.model('User', userSchema);
+// const Question = mongoose.model('Question', questionSchema);
+
+// const userSchema = mongoose.Schema({
+//   googleId: {type: String, required: true},
+//   name: {type: String, required: true},
+//   accessToken: {type: String, required: true},
+//   currentQuestion: {type: String},
+//   score: {type: Array}
+// });
+
+// userSchema.methods.apiRepr = function() {
+//   return {
+//     name: this.name
+//   };
+// };
+
+// const User = mongoose.model('User', userSchema);
 
 
-
-module.exports = {User};
+// module.exports = {Question, User};
