095d5730d59d11b19ec7f5f0b4e9313b232836cd
Saule BN <saulebohoney@gmail.com>
2017-10-25 10:33:51 -0400

    added css for header component

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,7 +4,7 @@ import './css/header.css';
 export default function Header() {
     return (
         <div className="header">
-            <h1>Job-Board-App</h1>
+            <h1><font color="white">Job-Board-App</font></h1>
         </div>
     );
 };
